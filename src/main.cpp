c882131d7d97bfde249f4adf1e2d53bafd3c9030
Maxim Pechyonkin <fazzolini@gmail.com>
2017-12-11 14:02:41 +0800

    Display yellow line in simulator; change delay to 0; refactor

--- src/main.cpp
+++ src/main.cpp
@@ -25,8 +25,9 @@ double rad2deg(double x) { return x * 180 / pi(); }
 // To avoid recalculating each time
 const double STEERING_MAX_LIMIT = 0.436332;
 // Delay
-const size_t DELAY_MILLISECONDS = 100;
-const double DELAY_SECONDS = static_cast<double>(DELAY_MILLISECONDS) / 1000.0;
+const size_t DELAY_MILLISECONDS = 0;
+const double DELAY_SECONDS = 0.0;
+//const double DELAY_SECONDS = static_cast<double>(DELAY_MILLISECONDS) / 1000.0;
 // This is the length from front to CoG that has a similar radius.
 const double LF = 2.67;
 
@@ -140,14 +141,18 @@ int main() {
           // To store points in car frame
           VectorXd waypoints_xs(N_WAYPOINTS);
           VectorXd waypoints_ys(N_WAYPOINTS);
+          // Pre compute sine and cosine of psi
+          const double cospsi = cos(-psi);
+          const double sinpsi = sin(-psi);
+          // Calculate coods relative to car frame
           for (int i = 0; i < N_WAYPOINTS; ++i) {
             // Affine transform: center and multiply by rotation matrix
             // Center point around current position of car
-            double px_centered = ptsx[i] - px;
-            double py_centered = ptsy[i] - py;
+            const double px_centered = ptsx[i] - px;
+            const double py_centered = ptsy[i] - py;
             // Multiply by rotation matrix
-            waypoints_xs[i] = px_centered * cos(-psi) - py_centered * sin(-psi);
-            waypoints_ys[i] = px_centered * sin(-psi) + py_centered * cos(-psi);
+            waypoints_xs[i] = px_centered * cospsi - py_centered * sinpsi;
+            waypoints_ys[i] = px_centered * sinpsi + py_centered * cospsi;
           }
 
           // 2. Fit polynomial to target waypoints
@@ -211,6 +216,11 @@ int main() {
 // ======================= MY IMPLEMENTATION | START ======================= //
 
           // TODO: Create vectors with target trajectory coordinates
+          // They are already calculated, just copy values
+          for (int k = 0; k < N_WAYPOINTS; ++k) {
+            next_x_vals.push_back(waypoints_xs[k]);
+            next_y_vals.push_back(waypoints_ys[k]);
+          }
 
 // ======================== MY IMPLEMENTATION | END ======================== //
 
